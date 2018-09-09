58c0b3e79ee677b276462772051cad44252deea3
shejian <shejian.gg@gmail.com>
2018-09-09 22:09:22 +0800

    增加了直接选择时间定位pager的弹窗

--- sample/src/main/java/com/prolificinteractive/materialcalendarview/sample/DialogsActivity.java
+++ sample/src/main/java/com/prolificinteractive/materialcalendarview/sample/DialogsActivity.java
@@ -2,6 +2,7 @@ package com.prolificinteractive.materialcalendarview.sample;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -9,11 +10,14 @@ import android.support.v7.app.AppCompatDialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
+
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
+
 import org.threeten.bp.format.DateTimeFormatter;
 
 /**
@@ -21,71 +25,89 @@ import org.threeten.bp.format.DateTimeFormatter;
  */
 public class DialogsActivity extends AppCompatActivity {
 
-  private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("EEE, d MMM yyyy");
-
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_dialogs);
-    ButterKnife.bind(this);
-  }
-
-  @OnClick(R.id.button_normal_dialog)
-  void onNormalDialogClick() {
-    new SimpleDialogFragment().show(getSupportFragmentManager(), "test-normal");
-  }
-
-  @OnClick(R.id.button_simple_dialog)
-  void onSimpleCalendarDialogClick() {
-    new SimpleCalendarDialogFragment().show(getSupportFragmentManager(), "test-simple-calendar");
-  }
-
-  public static class SimpleDialogFragment extends AppCompatDialogFragment {
-
-    @NonNull
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-      return new AlertDialog.Builder(getActivity())
-          .setTitle(R.string.title_activity_dialogs)
-          .setMessage("Test Dialog")
-          .setPositiveButton(android.R.string.ok, null)
-          .create();
-    }
-  }
-
-  public static class SimpleCalendarDialogFragment extends AppCompatDialogFragment
-      implements OnDateSelectedListener {
-
-    private TextView textView;
-
-    @NonNull
-    @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
-      LayoutInflater inflater = getActivity().getLayoutInflater();
-
-      //inflate custom layout and get views
-      //pass null as parent view because will be in dialog layout
-      View view = inflater.inflate(R.layout.dialog_basic, null);
-
-      textView = view.findViewById(R.id.textView);
-
-      MaterialCalendarView widget = view.findViewById(R.id.calendarView);
-
-      widget.setOnDateChangedListener(this);
-
-      return new AlertDialog.Builder(getActivity())
-          .setTitle(R.string.title_activity_dialogs)
-          .setView(view)
-          .setPositiveButton(android.R.string.ok, null)
-          .create();
-    }
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("EEE, d MMM yyyy");
 
     @Override
-    public void onDateSelected(
-        @NonNull MaterialCalendarView widget,
-        @NonNull CalendarDay date,
-        boolean selected) {
-      textView.setText(FORMATTER.format(date.getDate()));
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialogs);
+        ButterKnife.bind(this);
     }
-  }
+
+    @OnClick(R.id.button_normal_dialog)
+    void onNormalDialogClick() {
+        new SimpleDialogFragment().show(getSupportFragmentManager(), "test-normal");
+    }
+
+    @OnClick(R.id.button_simple_dialog)
+    void onSimpleCalendarDialogClick() {
+        new SimpleCalendarDialogFragment().show(getSupportFragmentManager(), "test-simple-calendar");
+    }
+
+    public static class SimpleDialogFragment extends AppCompatDialogFragment {
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.title_activity_dialogs)
+                    .setMessage("Test Dialog")
+                    .setPositiveButton(android.R.string.ok, null)
+                    .create();
+        }
+    }
+
+    public static class SimpleCalendarDialogFragment extends AppCompatDialogFragment
+            implements OnDateSelectedListener {
+
+        private TextView textView;
+        private OnDateSelectedListener mSelectedListener;
+
+        public void setmSelectedListener(OnDateSelectedListener mSelectedListener) {
+            this.mSelectedListener = mSelectedListener;
+        }
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            LayoutInflater inflater = getActivity().getLayoutInflater();
+
+            //inflate custom layout and get views
+            //pass null as parent view because will be in dialog layout
+            View view = inflater.inflate(R.layout.dialog_basic, null);
+
+            textView = view.findViewById(R.id.textView);
+
+            MaterialCalendarView widget = view.findViewById(R.id.calendarView);
+
+            widget.setOnDateChangedListener(this);
+
+            return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.title_activity_dialogs)
+                    .setView(view)
+                    .setPositiveButton(android.R.string.ok, (dialog, which) -> {
+                        if (mSelectedListener != null) {
+                            mSelectedListener.onDateSelected(widget, date, selected);
+                        }
+                    })
+                    .create();
+        }
+
+
+        MaterialCalendarView widget;
+        CalendarDay date;
+        boolean selected;
+
+        @Override
+        public void onDateSelected(
+                @NonNull MaterialCalendarView widget,
+                @NonNull CalendarDay date,
+                boolean selected) {
+            this.date = date;
+            this.widget = widget;
+            this.selected = selected;
+            textView.setText(FORMATTER.format(date.getDate()));
+        }
+
+    }
 }
